5ea676a3ca1fad05ed36df9b64b03f1b7fc81d69
tkburis <tkburis@gmail.com>
2022-12-02 22:14:50 +0000

    Added code submission

--- website/problem.py
+++ website/problem.py
@@ -1,8 +1,24 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request
 
 problem_bp = Blueprint('problem_bp', __name__)
 
 # Load problem main screen.
 @problem_bp.route('/<int:id>')
 def problem(id):
-    return render_template('problem.html', problem_id=id)
+    
+    # This is hardcoded for development purposes.
+    # `problem` in production will be an object from the problem database.
+    problem_dict = {
+        'id': 1,
+        'title': 'Sum',
+        'description': 'Given two numbers, print their sum.',
+    }
+    
+    return render_template('problem.html', problem=problem_dict)
+
+# Code submission.
+@problem_bp.route('/submit', methods=['POST'])
+def problem_submit():
+    user_code = request.form['user_code']
+    
+    return user_code
