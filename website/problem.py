edb309b51a72080dbbd5ec42765635d76a4320ad
tkburis <tkburis@gmail.com>
2022-12-02 21:36:49 +0000

    Added comments

--- website/problem.py
+++ website/problem.py
@@ -2,6 +2,7 @@ from flask import Blueprint, render_template
 
 problem_bp = Blueprint('problem_bp', __name__)
 
+# Load problem main screen.
 @problem_bp.route('/<int:id>')
 def problem(id):
     return render_template('problem.html', problem_id=id)
