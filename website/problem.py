7f0dfdfe705b7a9a2852340afda70dc5777e5c35
tkburis <tkburis@gmail.com>
2022-12-04 14:22:54 +0000

    Planned submission flow

--- website/problem.py
+++ website/problem.py
@@ -21,7 +21,9 @@ def problem(id: int) -> str:
 @problem_bp.route('/results', methods=['GET', 'POST'])
 def problem_results() -> str:
     # TODO: After submission, the whole result should be stored into a database, and should redirect to /submission/<submission-id> instead.
-    # /results with POST is only temporarily used for development purposes. 
+    # Maybe should look like:
+    # Submit code --(POST)--> /submit (judge + add to DB) --(redirect with submission ID)--> /submission/<id>
+    # ??
 
     # Note this is hardcoded for development.
     # Probably get this from `problem_id` in production.
