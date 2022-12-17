f02b4d536260580f284baac8caec5ae72da624b1
TK Buristrakul <tkburis@gmail.com>
2022-12-17 09:49:33 +0700

    submission html

--- website/submission/routes.py
+++ website/submission/routes.py
@@ -5,16 +5,7 @@ from isolate_wrapper import Verdict, Result
 
 submission_bp = Blueprint('submission', __name__, template_folder='templates', static_folder='static')
 
-@submission_bp.route('/<id>')
+@submission_bp.route('/<int:id>')
 def submission(id: int) -> str:
-    submission: Submission = Submission(
-        username='fuco1',
-        final_verdict=Verdict.AC,
-        results=[
-            Result(Verdict.AC, 400, 64*1024),
-            Result(Verdict.AC, 390, 64*1024),
-        ],
-        problem_id='A1',
-        assignment_id=1,
-    )
-    return render_template('submission.html', submission=submission)
+    submission_obj = Submission.find_one({'id': id})
+    return render_template('submission.html', submission=submission_obj)
