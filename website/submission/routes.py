7c1ca20c25934194a0df66400dd4cc90c74c95e9
TK Buristrakul <tkburis@gmail.com>
2023-01-09 22:55:45 +0000

    submission username

--- website/submission/routes.py
+++ website/submission/routes.py
@@ -1,7 +1,7 @@
 from flask import Blueprint, render_template, request, abort
 from flask_login import login_required, current_user
 
-from website.models import Submission
+from website.models import Submission, User
 from isolate_wrapper import Verdict, Result
 
 submission_bp = Blueprint('submission_bp', __name__, template_folder='templates', static_folder='static')
@@ -12,5 +12,6 @@ def submission(id: int) -> str:
     submission_obj = Submission.find_one({'id': id})
     if submission_obj is None:
         abort(404, description="Submission not found")
+    user_obj = User.find_one({'id': submission_obj.user_id})
     show_code = current_user.is_admin or current_user.id == submission_obj.user_id or submission_obj.problem_id in current_user.get_solved_problem_ids()
-    return render_template('submission.html', submission=submission_obj, show_code=show_code)
+    return render_template('submission.html', submission=submission_obj, submission_user=user_obj, show_code=show_code)
