d092ff5bea2d0f9fea073b270326bfb43c2234d7
L3ungj <justin.leungj@gmail.com>
2023-01-13 21:53:05 +0000

    error descriptions

--- website/errors.py
+++ website/errors.py
@@ -1,8 +1,8 @@
 from flask import render_template
 from flask.typing import ResponseReturnValue
 
-def page_not_found(e: int) -> ResponseReturnValue:
-    return render_template('404.html'), 404
+def page_not_found(e) -> ResponseReturnValue:
+    return render_template('404.html', description=e.description), 404
 
-def unauthorised(e: int):
-    return render_template('403.html'), 403
+def unauthorised(e):
+    return render_template('403.html', description=e.description), 403
