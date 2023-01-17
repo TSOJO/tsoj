5f7ee2bf6971f55cab9d9f3d5370e5868fa4ca63
Eden <cheungedenhk@gmail.com>
2023-01-17 22:41:32 +0000

    Fix broken stuff

--- website/user/static/script.js
+++ website/user/static/script.js
@@ -3,7 +3,7 @@
     'use strict'
     
     // Fetch all the forms we want to apply custom Bootstrap validation styles to
-    const forms = $('.needs-validation')
+    const forms = document.querySelectorAll('.needs-validation')
     
     // Loop over them and prevent submission
     Array.from(forms).forEach(form => {
