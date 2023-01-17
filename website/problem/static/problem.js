0d5c8ba0e2aa70e1424de28a11b49a806dff5e8b
TK Buristrakul <tkburis@gmail.com>
2023-01-17 16:07:11 +0000

    change wording when submit

--- website/problem/static/problem.js
+++ website/problem/static/problem.js
@@ -25,7 +25,7 @@ window.onpageshow = function(event) {
         $(this).prop('disabled', true);
         // Replace text with a spinner.
         $(this).html(
-            '<span class="spinner-border spinner-border-sm code-submit" role="status" aria-hidden="true"></span> Running...'
+            '<span class="spinner-border spinner-border-sm code-submit" role="status" aria-hidden="true"></span> Submitting...'
         );
         // Submit form.
         $('#codeForm').submit();
