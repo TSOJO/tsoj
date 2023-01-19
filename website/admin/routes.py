cbb00b0540ed1528ee6073007b4095a0e0009ea9
L3ungj <justin.leungj@gmail.com>
2023-01-19 19:37:07 +0000

    fix user group bugs

--- website/admin/routes.py
+++ website/admin/routes.py
@@ -227,7 +227,7 @@ def create_user_group():
         if user_ids_raw:
             user_ids = user_ids_raw.split(',')
             user_group.user_ids = user_ids
-        user_group.save(wait=True)
+        user_group.save(wait=True, replace=True)
         flash('Group created', 'success')
         return redirect(url_for('admin_bp.user_groups'))
     users = User.find_all()
@@ -257,8 +257,8 @@ def delete_user_group(id: int):
     flash('Group deleted', 'success')
     return redirect(url_for('admin_bp.user_groups'))
 
+# Other
 
-# ! Do we need this??
 @admin_bp.route('/delete/submission/<int:id>')
 def delete_submission(id: int):
     submission = Submission.find_one({'id': id})
