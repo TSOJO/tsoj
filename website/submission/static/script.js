33b79f414aac198bc7a52b19781156ff76eeb7a2
TK Buristrakul <tkburis@gmail.com>
2023-01-30 08:27:05 +0000

    restyle of error message

--- website/submission/static/script.js
+++ website/submission/static/script.js
@@ -71,14 +71,20 @@ function update_results(results) {
             memory_node.html(results[i].memory + ' KB')
         }
         if (results[i].message) {
-            details_node.html(['<a data-bs-toggle="modal" data-bs-target="#detail' + (i+1) + '-modal" href="#"',
-                '    class="text-decoration-none">Details</a>',
+            verdict_node.append([
+                '<a data-bs-toggle="modal" data-bs-target="#detail' + (i+1) + '-modal" href="#"',
+                '   class="text-decoration-none">',
+                '   <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="#ff6a00" class="bi bi-exclamation-circle" viewBox="0 0 16 16">',
+                '      <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/>',
+                '      <path d="M7.002 11a1 1 0 1 1 2 0 1 1 0 0 1-2 0zM7.1 4.995a.905.905 0 1 1 1.8 0l-.35 3.507a.552.552 0 0 1-1.1 0L7.1 4.995z"/>',
+                '   </svg>',
+                '</a>',
                 '<div class="modal fade" id="detail' + (i+1) + '-modal" tabindex="-1"',
                 '    aria-labelledby="detail' + (i+1) + '-modal-label" aria-hidden="true">',
                 '    <div class="modal-dialog">',
                 '        <div class="modal-content">',
                 '            <div class="modal-header">',
-                '                <h1 class="modal-title fs-5" id="detail' + (i+1) + '-modal-label">Error message',
+                '                <h1 class="modal-title fs-5" id="detail' + (i+1) + '-modal-label">' + results[i].verdict.verdict_long,
                 '                </h1>',
                 '                <button type="button" class="btn-close" data-bs-dismiss="modal"',
                 '                    aria-label="Close"></button>',
@@ -94,7 +100,8 @@ function update_results(results) {
                 '            </div>',
                 '        </div>',
                 '    </div>',
-                '</div>',].join(''))
+                '</div>',
+            ].join(''))
         }
     }
 }
