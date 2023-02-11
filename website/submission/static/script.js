b60b372980a9f78ba81fea3d4110b947946be106
L3ungj <justin.leungj@gmail.com>
2023-02-11 16:09:20 +0000

    AQA assembly lang

--- website/submission/static/script.js
+++ website/submission/static/script.js
@@ -111,7 +111,7 @@ window.onpageshow = () => {
     let editor = ace.edit('editor')
     ace.config.set('basePath', 'https://cdn.jsdelivr.net/npm/ace-builds@1.13.1/src-noconflict/')
     editor.setTheme("ace/theme/textmate")
-    editor.session.setMode("ace/mode/python")
+    editor.session.setMode("ace/mode/" + getAceMode($('#language-name').val()))
     editor.session.setUseWrapMode(true)
     editor.setOptions({ readOnly: true, highlightActiveLine: false, highlightGutterLine: false, maxLines: Infinity })
     editor.renderer.$cursorLayer.element.style.display = "none"
