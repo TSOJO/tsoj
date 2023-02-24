bdd9d574114f1be68e9bafc189ef45f53bd12032
TK Buristrakul <tkburis@gmail.com>
2023-02-24 17:29:33 +0000

    submission custom search

--- website/home/static/script.js
+++ website/home/static/script.js
@@ -1,8 +1,22 @@
-function problem_searcher(data, text) {
+function contains(object, keys, text) {
     lowered_text = text.toLowerCase()
+    for (const key of keys) {
+        if (object[key].toString().toLowerCase().includes(lowered_text)) {
+            return true
+        }
+    }
+    return false
+}
+
+function problem_searcher(data, text) {
     return data.filter(row => {
-        return row._data.id.toString().toLowerCase().includes(lowered_text) ||
-               row._data.name.toString().toLowerCase().includes(lowered_text) ||
-               row._data.solves.toString().toLowerCase().includes(lowered_text)
+        return contains(row._data, ['id', 'name', 'solves'], text)
     })
 }
+
+function submission_searcher(data, text) {
+    return data.filter(row => {
+        return contains(row._data, ['time', 'username', 'problem', 'language', 'verdict'], text)
+    })
+}
+
