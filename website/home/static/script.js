69747f71f41aecdb7eac6f28f608821bcd263dae
TK Buristrakul <tkburis@gmail.com>
2023-02-24 17:10:35 +0000

    fix search for problems

---
+++ website/home/static/script.js
@@ -0,0 +1,8 @@
+function problem_searcher(data, text) {
+    lowered_text = text.toLowerCase()
+    return data.filter(row => {
+        return row._data.id.toString().toLowerCase().includes(lowered_text) ||
+               row._data.name.toString().toLowerCase().includes(lowered_text) ||
+               row._data.solved.toString().toLowerCase().includes(lowered_text)
+    })
+}
